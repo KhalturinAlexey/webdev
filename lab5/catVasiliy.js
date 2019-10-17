db0896264434dff92130548631ffc6e4c97f2b16
Alexey Khalturin <AleshkaXal@rambler.ru>
2019-10-18 00:38:16 +0300

    add reaction for positive events

--- lab5/catVasiliy.js
+++ lab5/catVasiliy.js
@@ -2,6 +2,7 @@ var catVasiliy = {
   name: 'Василий',
   birthday: new Date(),
   listOfFears: ['vacuum cleaner', 'beep'],
+  listOfJoys: ['ksksks', 'Vasya'],
   meow: function () {
     console.log('Meow!');
   },
@@ -9,6 +10,11 @@ var catVasiliy = {
     if (this.listOfFears.indexOf(feeling) > -1) {
       this.meow();
       console.log('run from here!!!!')
+    }
+    if (this.listOfJoys.indexOf(feeling) > -1) {
+      for (var i = 0; i < 5; i++) {
+        this.meow();
+      }
     }
   },
 };
