9527e2387f3b43a1836e04e150974989f98c63a3
Alexey Khalturin <AleshkaXal@rambler.ru>
2019-10-20 02:11:04 +0300

    add vertical navigation

--- lab4/total-commander/js/total-commander.js
+++ lab4/total-commander/js/total-commander.js
@@ -9,14 +9,10 @@ function select(element) {
   }
 }
 
-function clearSelection() {
-  var selection = document.querySelectorAll('.selected');
-  selection.forEach(element => element.classList.remove('selected'));
-}
-
 window.onload = function() {
   var leftPanelItems = document.querySelectorAll('.panel-left .row');
   var rightPanelItems = document.querySelectorAll('.panel-right .row');
+  var isLeftPanelSelected = true;
 
   var leftPanelSelectedIndex = 0;
   var rightPanelSelectedIndex = 0;
@@ -24,18 +20,43 @@ window.onload = function() {
   select(leftPanelItems[0]);
 
   document.onkeydown = function checkKey(e) {
+    if (e.keyCode == arrowLeftKeyCode || e.keyCode == arrowUpKeyCode ||
+        e.keyCode == arrowRightKeyCode || e.keyCode ==  arrowDownKeyCode) {
+        var selection = document.querySelectorAll('.selected');
+        selection.forEach(element => element.classList.remove('selected'));
+    }
     switch (e.keyCode) {
       case arrowLeftKeyCode:
-        clearSelection();
+        isLeftPanelSelected = true;
         select(leftPanelItems[leftPanelSelectedIndex]);
         break;
       case arrowUpKeyCode:
+        if (isLeftPanelSelected && leftPanelSelectedIndex > 0) {
+          leftPanelSelectedIndex--;
+        } else if (!isLeftPanelSelected && rightPanelSelectedIndex > 0) {
+          rightPanelSelectedIndex--;
+        }
+        if (isLeftPanelSelected) {
+          select(leftPanelItems[leftPanelSelectedIndex]);
+        } else {
+          select(rightPanelItems[rightPanelSelectedIndex]);
+        }
         break;
       case arrowRightKeyCode:
-        clearSelection();
+        isLeftPanelSelected = false;
         select(rightPanelItems[rightPanelSelectedIndex]);
         break;
       case arrowDownKeyCode:
+        if (isLeftPanelSelected && leftPanelSelectedIndex < leftPanelItems.length - 1) {
+          leftPanelSelectedIndex++;
+        } else if (!isLeftPanelSelected && rightPanelSelectedIndex < rightPanelItems.length - 1) {
+          rightPanelSelectedIndex++;
+        }
+        if (isLeftPanelSelected) {
+          select(leftPanelItems[leftPanelSelectedIndex]);
+        } else {
+          select(rightPanelItems[rightPanelSelectedIndex]);
+        }
         break;
       default:
         break;
