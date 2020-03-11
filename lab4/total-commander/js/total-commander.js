4c30cfc60eb73a69715d763b4aa1892d34992c2a
KhalturinAlexey <aleshkaxal@rambler.ru>
2020-03-11 19:32:08 +0300

    Добавил prettier/formatter

--- lab4/total-commander/js/total-commander.js
+++ lab4/total-commander/js/total-commander.js
@@ -4,14 +4,14 @@ var arrowRightKeyCode = 39;
 var arrowDownKeyCode = 40;
 
 function select(element) {
-  if (element && !element.classList.contains('selected')) {
-    element.classList.add('selected');
+  if (element && !element.classList.contains("selected")) {
+    element.classList.add("selected");
   }
 }
 
 window.onload = function() {
-  var leftPanelItems = document.querySelectorAll('.panel-left .row');
-  var rightPanelItems = document.querySelectorAll('.panel-right .row');
+  var leftPanelItems = document.querySelectorAll(".panel-left .row");
+  var rightPanelItems = document.querySelectorAll(".panel-right .row");
   var isLeftPanelSelected = true;
 
   var leftPanelSelectedIndex = 0;
@@ -20,10 +20,14 @@ window.onload = function() {
   select(leftPanelItems[0]);
 
   document.onkeydown = function checkKey(e) {
-    if (e.keyCode == arrowLeftKeyCode || e.keyCode == arrowUpKeyCode ||
-        e.keyCode == arrowRightKeyCode || e.keyCode ==  arrowDownKeyCode) {
-        var selection = document.querySelectorAll('.selected');
-        selection.forEach(element => element.classList.remove('selected'));
+    if (
+      e.keyCode == arrowLeftKeyCode ||
+      e.keyCode == arrowUpKeyCode ||
+      e.keyCode == arrowRightKeyCode ||
+      e.keyCode == arrowDownKeyCode
+    ) {
+      var selection = document.querySelectorAll(".selected");
+      selection.forEach(element => element.classList.remove("selected"));
     }
 
     switch (e.keyCode) {
@@ -37,22 +41,36 @@ window.onload = function() {
         } else if (!isLeftPanelSelected && rightPanelSelectedIndex > 0) {
           rightPanelSelectedIndex--;
         }
-        select(isLeftPanelSelected ? leftPanelItems[leftPanelSelectedIndex] : rightPanelItems[rightPanelSelectedIndex]);
+        select(
+          isLeftPanelSelected
+            ? leftPanelItems[leftPanelSelectedIndex]
+            : rightPanelItems[rightPanelSelectedIndex]
+        );
         break;
       case arrowRightKeyCode:
         isLeftPanelSelected = false;
         select(rightPanelItems[rightPanelSelectedIndex]);
         break;
       case arrowDownKeyCode:
-        if (isLeftPanelSelected && leftPanelSelectedIndex < leftPanelItems.length - 1) {
+        if (
+          isLeftPanelSelected &&
+          leftPanelSelectedIndex < leftPanelItems.length - 1
+        ) {
           leftPanelSelectedIndex++;
-        } else if (!isLeftPanelSelected && rightPanelSelectedIndex < rightPanelItems.length - 1) {
+        } else if (
+          !isLeftPanelSelected &&
+          rightPanelSelectedIndex < rightPanelItems.length - 1
+        ) {
           rightPanelSelectedIndex++;
         }
-        select(isLeftPanelSelected ? leftPanelItems[leftPanelSelectedIndex] : rightPanelItems[rightPanelSelectedIndex]);
+        select(
+          isLeftPanelSelected
+            ? leftPanelItems[leftPanelSelectedIndex]
+            : rightPanelItems[rightPanelSelectedIndex]
+        );
         break;
       default:
         break;
     }
-  }
-}
+  };
+};
